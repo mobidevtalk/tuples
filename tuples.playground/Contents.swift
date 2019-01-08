b333869a457f76d78d63d1f2264dddbfaf3dc281
Shad Mazumder <shadmazumder@gmail.com>
2019-01-08 22:55:24 +0600

    Adding tuple.

---
+++ tuples.playground/Contents.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+/*:
+ tuple declaration and usage
+ */
+
+let greetings = ("Hola", "Spanish")
+greetings.0
+
+let organisedGreetings = (phrase: "Hej", locale: "Swedish")
+organisedGreetings.phrase
+
+/*:
+ When to use
+ */
+func info()->(name: String, age: Int){
+    return ("mobidevtalk", 0)
+}
+
+let mobiDevTalkInfo = info()
+mobiDevTalkInfo.name
+mobiDevTalkInfo.age
+
+/*:
+ Why to use
+ */
+
+struct Info{
+    let name: String
+    let age: Int
+}
+
+func modifiedInfo() -> Info{
+    return Info(name: "mobidevtalk", age: 0)
+}
+
+/*:
+ Difference from typealias
+ */
+
+typealias Amount = Int
+typealias Execute = (Int)->(String)
+
+typealias Age = (month: Int, year: Int)
+
+struct Profile{
+    let name: String
+    let age: Age
+    
+}
+
+let profile = Profile(name: "mobidevtalk", age: Age(month: 4, year: 0))
+profile.age.month
+
+let shortProfile = Profile(name: "mobidevtalk", age: (4, 0))
+shortProfile.age.year
+
